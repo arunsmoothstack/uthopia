3654dcd8fe2214674123eeec0683e21ac604333d
amavumkal <av8r88@gmail.com>
2020-12-19 21:59:08 -0500

    Added content negotiation for xml and jason

--- src/main/java/com/ss/uthopia/user/controller/UserController.java
+++ src/main/java/com/ss/uthopia/user/controller/UserController.java
@@ -5,25 +5,25 @@ import com.ss.uthopia.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/users")
+//@RequestMapping("api/users")
 public class UserController {
 
-    private final UserService USERS_SERVICE;
+    private UserService USERS_SERVICE;
     UserController(UserService userService) {
         this.USERS_SERVICE = userService;
     }
 
-    @GetMapping("/all")
-    public List<User> findUsers() {
+    @GetMapping(value = "api/users/all", produces = {"application/json", "application/xml"})
+    @ResponseBody
+    public Iterable<User> findUsers() {
         return USERS_SERVICE.findAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("api/users/{id}")
     public ResponseEntity<User> findById(@PathVariable long id) {
         Optional<User> users= USERS_SERVICE.findById(id);
         if(!users.isPresent()) {
@@ -32,7 +32,7 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(users.get());
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("api/users/{id}")
     public ResponseEntity<Long> deleteUser(@PathVariable long id) {
         USERS_SERVICE.deleteById(id);
         return ResponseEntity.status(HttpStatus.OK).body(id);
--- src/main/java/com/ss/uthopia/user/service/UserService.java
+++ src/main/java/com/ss/uthopia/user/service/UserService.java
@@ -2,6 +2,7 @@ package com.ss.uthopia.user.service;
 
 import com.ss.uthopia.user.dao.UserDao;
 import com.ss.uthopia.user.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,14 +10,16 @@ import java.util.Optional;
 
 @Service
 public class UserService {
-
+    @Autowired
     private UserDao userDao;
-    public UserService(UserDao userDao) {
-        this.userDao = userDao;
-    }
 
-    public List<User> findAll() {
-        return (List<User>) userDao.findAll();
+//    public UserService() {
+//        //this.userDao = userDao;
+//    }
+
+
+    public Iterable<User> findAll() {
+        return  userDao.findAll();
     }
 
     public Optional<User> findById(long id) {
